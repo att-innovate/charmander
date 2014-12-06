aee7ceacd0b85da8d6380079bd7d3141084307f8
Marcel Neuhausler <neuhausler@gmail.com>
2014-12-05 23:09:01 -0800

    Added influxdb

--- bin/invm/build_analytics.sh
+++ bin/invm/build_analytics.sh
@@ -5,10 +5,21 @@ ANALYTICS_DIR=/vagrant/analytics
 ANALYTICS_DATA_DIR=/data
 
 mkdir -p $ANALYTICS_DATA_DIR/redis
+mkdir -p $ANALYTICS_DATA_DIR/influxdb
+
 chown -R 777 $ANALYTICS_DATA_DIR
+
 
 REDIS_DIR=$ANALYTICS_DIR/redis
 cd $REDIS_DIR &&
     image_name="redis" &&
 	echo "Building $image_name" &&
 	docker build -t $image_name .
+
+
+INFLUXDB_DIR=$ANALYTICS_DIR/influxdb
+cd $INFLUXDB_DIR &&
+    image_name="influxdb" &&
+	echo "Building $image_name" &&
+	docker build -t $image_name .
+
